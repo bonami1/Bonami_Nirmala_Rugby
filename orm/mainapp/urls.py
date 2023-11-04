395c5b8e61026e6603d94618647800ef9fc84b53
Nirmala <nirmalabonami@gmail.com>
2023-11-04 13:08:47 +0100

    ajout fichier about.css

--- orm/mainapp/urls.py
+++ orm/mainapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 # Import des URLs de l'interface d'administration
 from django.contrib import admin
 # Import des vues qui sont déclarées dans leur propre module (dossier)
-from .views import HomeView, StadiumsView, TeamsView, NewsletterView, UpdateView
+from .views import HomeView, StadiumsView, TeamsView, NewsletterView, UpdateView, AboutView
 
 urlpatterns = (
     path("", HomeView.as_view(), name="home"),
@@ -10,6 +10,7 @@ urlpatterns = (
     path("teams", TeamsView.as_view(), name="teams"),
     path("newsletter", NewsletterView.as_view(), name="newsletter"),
     path("update", UpdateView.as_view(), name="update"),
+    path("about", AboutView.as_view(), name="about"),
     # Dans un cadre de projet réel, il serait préférable d'utiliser une URL moins prévisible que "admin"
     path("admin", admin.site.urls),
 )
